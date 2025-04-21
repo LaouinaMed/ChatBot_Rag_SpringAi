f6fa9257019904b757eaf61312703dfcb8b8e9d8
Mohammed Laouina <simed.laouina@gmail.com>
2025-04-21 16:42:18 +0100

    integration api openAi

---
+++ src/test/java/com/example/chatbot_rag/ChatBotRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatbot_rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
